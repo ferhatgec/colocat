5c50036ea578c602dfe101e9301a611031008974
ferhatgec <ferhatgectao@gmail.com>
2020-12-19 00:59:13 +0300

    Suppress: Run after installing.

--- install.sh
+++ install.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-g++ -std=c++17 -I./Library/ -I./include/ src/Languages.cpp src/ColoCat.cpp -o /bin/colocat 
-
-colocat
+g++ -std=c++17 -I./Library/ -I./include/ src/Languages.cpp src/ColoCat.cpp -o /bin/colocat
